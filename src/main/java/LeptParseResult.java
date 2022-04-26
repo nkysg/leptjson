9f22d9d999ed7a9b8f970518b67b972401f38f1f
nk_ysg <nk_ysg@163.com>
2022-04-26 17:20:52 +0800

    add parse number

--- src/main/java/LeptParseResult.java
+++ src/main/java/LeptParseResult.java
@@ -1,16 +1,17 @@
-public enum LeptParseError {
+public enum LeptParseResult {
     LEPT_PARSE_OK(0),
     LEPT_PARSE_EXPECT_VALUE(1),
     LEPT_PARSE_INVALID_VALUE(2),
-    LEPT_PARSE_ROOT_NOT_SINGULAR(3);
+    LEPT_PARSE_ROOT_NOT_SINGULAR(3),
+    LEPT_PARSE_NUMBER_TOO_BIG(4);
 
     private int code;
 
-    LeptParseError(int code) {
+    LeptParseResult(int code) {
         this.code = code;
     }
 
-    int getLeptParseError() {
+    int getLeptParseResult() {
         return code;
     }
 }
