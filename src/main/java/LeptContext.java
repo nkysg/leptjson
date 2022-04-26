4c135878abb1e62d84d636b8bfd134c1d373fdd1
nk_ysg <nk_ysg@163.com>
2022-04-26 17:42:28 +0800

    1.refactor lept_parse_literal

--- src/main/java/LeptContext.java
+++ src/main/java/LeptContext.java
@@ -48,6 +48,20 @@ public class LeptContext {
         return result;
     }
 
+    private LeptParseResult lept_parse_literal(LeptValue value, String expect, LeptType type) {
+
+        for (char ch : expect.toCharArray()) {
+            if (ch != next()) {
+                return LeptParseResult.LEPT_PARSE_INVALID_VALUE;
+            }
+        }
+        value.setLeptType(type);
+        if (hasNext()) {
+            return LeptParseResult.LEPT_PARSE_ROOT_NOT_SINGULAR;
+        }
+        return LeptParseResult.LEPT_PARSE_OK;
+    }
+
     private LeptParseResult lept_parse_value(LeptValue value) {
         if (!hasNext()) {
             return LeptParseResult.LEPT_PARSE_EXPECT_VALUE;
@@ -55,51 +69,18 @@ public class LeptContext {
         char ch = peek();
         switch (ch) {
             case 'n':
-                return lept_parse_null(value);
+                return lept_parse_literal(value, "null", LeptType.LEPT_NULL);
             case 't':
-                return lept_parse_true(value);
+                return lept_parse_literal(value, "true", LeptType.LEPT_TRUE);
             case 'f':
-                return lept_parse_false(value);
-           // case '"':
-             //   return lept_parse_string(value);
+                return lept_parse_literal(value, "false", LeptType.LEPT_FALSE);
+            case '"':
+                return lept_parse_string(value);
             case NONE:
                 return LeptParseResult.LEPT_PARSE_EXPECT_VALUE;
             default:
                 return lept_parse_number(value);
         }
-    }
-
-    private LeptParseResult lept_parse_null(LeptValue value) {
-        if (next() !='n' || next() != 'u' || next() !='l' || next() != 'l') {
-            return LeptParseResult.LEPT_PARSE_INVALID_VALUE;
-        }
-        value.setLeptType(LeptType.LEPT_NULL);
-        if (hasNext()) {
-            return LeptParseResult.LEPT_PARSE_ROOT_NOT_SINGULAR;
-        }
-        return LeptParseResult.LEPT_PARSE_OK;
-    }
-
-    private LeptParseResult lept_parse_true(LeptValue value) {
-        if (next() !='t' || next() != 'r' || next() != 'u' || next() != 'e') {
-            return LeptParseResult.LEPT_PARSE_INVALID_VALUE;
-        }
-        value.setLeptType(LeptType.LEPT_TRUE);
-        if (hasNext()) {
-            return LeptParseResult.LEPT_PARSE_ROOT_NOT_SINGULAR;
-        }
-        return LeptParseResult.LEPT_PARSE_OK;
-    }
-
-    private LeptParseResult lept_parse_false(LeptValue value) {
-        if (next() !='f' || next() != 'a' || next() != 'l' || next() != 's' || next() != 'e') {
-            return LeptParseResult.LEPT_PARSE_INVALID_VALUE;
-        }
-        value.setLeptType(LeptType.LEPT_FALSE);
-        if (hasNext()) {
-            return LeptParseResult.LEPT_PARSE_ROOT_NOT_SINGULAR;
-        }
-        return LeptParseResult.LEPT_PARSE_OK;
     }
 
     private LeptParseResult lept_parse_number(LeptValue value) {
@@ -143,6 +124,10 @@ public class LeptContext {
         }
         value.setLeptType(LeptType.LEPT_NUMBER);
         value.setLeptDouble(json.substring(start_idx, pos));
+        return LeptParseResult.LEPT_PARSE_OK;
+    }
+
+    private LeptParseResult lept_parse_string(LeptValue value) {
         return LeptParseResult.LEPT_PARSE_OK;
     }
 }
